d5d1cb8c5bdd5e057a49e0332bd0994cf0c4d8d8
mikethevrguy <40547376+mikethevrguy@users.noreply.github.com>
2022-11-16 14:48:31 -0700

    prepared student project for VRIF

---
+++ Assets/Scripts/StudentScripts/BallSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    
+
+    public GameObject pooledBall; //the prefab of the object in the object pool
+
+    private void Awake()
+    {
+        
+    }
+    private void Start()
+    {
+        InvokeRepeating("SpawnBall", 0f, 0.5f);
+    }
+    
+   	
+	// Update is called once per frame
+	private void Update () 
+    {
+	
+	}
+
+    void SpawnBall()
+    {
+        Instantiate(pooledBall);
+    }
+}
